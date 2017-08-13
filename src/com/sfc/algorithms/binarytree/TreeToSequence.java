768e38f4817e92c57042e81a66608635c3f9f162
Prophet Xu <slowfishcolor@gmail.com>
2017-08-13 22:55:33 +0800

    TreeToSequence using recrusion to traversal a binary tree

---
+++ src/com/sfc/algorithms/binarytree/TreeToSequence.java
@@ -0,0 +1,26 @@
+package com.sfc.algorithms.binarytree;
+
+/**
+ *
+ * 请用递归方式实现二叉树的先序、中序和后序的遍历打印。
+ *
+ * 给定一个二叉树的根结点root，请依次返回二叉树的先序，中序和后续遍历(二维数组的形式)
+ *
+ * Created by Prophet on 2017/8/13.
+ */
+public class PrintBinaryTree {
+
+    private static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    
+
+    private static void printFirst
+}
