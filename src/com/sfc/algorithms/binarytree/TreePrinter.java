9764e55249d936920e48515a0cd822328cbcb3b1
Prophet Xu <slowfishcolor@gmail.com>
2017-08-18 13:22:39 +0800

    commit

--- src/com/sfc/algorithms/binarytree/TreePrinter.java
+++ src/com/sfc/algorithms/binarytree/TreePrinter.java
@@ -1,7 +1,68 @@
 package com.sfc.algorithms.binarytree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
+ * 有一棵二叉树，请设计一个算法，按照层次打印这棵二叉树。
+ * 给定二叉树的根结点root，请返回打印结果，结果按照每一层一个数组进行储存，所有数组的顺序按照层数从上往下，且每一层的数组内元素按照从左往右排列。保证结点数小于等于500。
+ *
  * Created by Prophet on 2017/8/15.
  */
 public class TreePrinter {
+
+    private static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int[][] printTree(TreeNode root) {
+        // last 记录当前行的末尾节点，nlast 记录下一行的末尾节点, bfs 队列遍历
+        Queue<TreeNode> queue = new LinkedList<>();
+        // 保存当前层
+        ArrayList<Integer> layer = new ArrayList<>();
+        // 保存结果
+        ArrayList<List<Integer>> resultList = new ArrayList<>();
+        TreeNode last = root;
+        TreeNode nlast = null;
+        TreeNode node = root;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            layer.add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+                nlast = node.left;
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+                nlast = node.right;
+            }
+            if (last == node) {
+                last = nlast;
+                resultList.add(layer);
+                layer = new ArrayList<>();
+            }
+        }
+
+        // 转数组
+        int[][] result = new int[resultList.size()][];
+        int i = 0;
+        for (List<Integer> list: resultList) {
+            int[] temp = new int[list.size()];
+            for (int j = 0; j < list.size(); j++) {
+                temp[j] = list.get(j);
+            }
+            result[i++] = temp;
+        }
+
+        return result;
+    }
 }
--- src/com/sfc/algorithms/binarytree/TreeToSequence.java
+++ src/com/sfc/algorithms/binarytree/TreeToSequence.java
@@ -1,5 +1,8 @@
 package com.sfc.algorithms.binarytree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * 请用递归方式实现二叉树的先序、中序和后序的遍历打印。
@@ -8,7 +11,7 @@ package com.sfc.algorithms.binarytree;
  *
  * Created by Prophet on 2017/8/13.
  */
-public class PrintBinaryTree {
+public class TreeToSequence {
 
     private static class TreeNode {
         int val = 0;
@@ -20,7 +23,73 @@ public class PrintBinaryTree {
         }
     }
 
-    
+    public static int[][] convert(TreeNode root) {
+        List<Integer> list1 = new LinkedList<>();
+        List<Integer> list2 = new LinkedList<>();
+        List<Integer> list3 = new LinkedList<>();
+        traversal_1(root, list1);
+        traversal_2(root, list2);
+        traversal_3(root, list3);
+        int[][] result = new int[3][list1.size()];
+        for (int i = 0; i < list1.size(); i++) {
+            result[0][i] = list1.get(i);
+            result[1][i] = list2.get(i);
+            result[2][i] = list3.get(i);
+        }
+        return result;
+    }
 
-    private static void printFirst
+    private static void traversal_1(TreeNode root, List<Integer> list) {
+        // 先序遍历
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        traversal_1(root.left, list);
+        traversal_1(root.right, list);
+    }
+
+    private static void traversal_2(TreeNode root, List<Integer> list) {
+        // 中序遍历
+        if (root == null) {
+            return;
+        }
+        traversal_2(root.left, list);
+        list.add(root.val);
+        traversal_2(root.right, list);
+    }
+
+    private static void traversal_3(TreeNode root, List<Integer> list) {
+        // 后序遍历
+        if (root == null) {
+            return;
+        }
+        traversal_3(root.left, list);
+        traversal_3(root.right, list);
+        list.add(root.val);
+    }
+
+    public static void main(String args[]) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+
+        int[][] result = convert(node1);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
